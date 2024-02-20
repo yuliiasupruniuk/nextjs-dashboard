8ea2473bea736e9c1ac3dcea6855e0ca5c9b789f
Yuliia <yuliia.supruniuk@kapsys.at>
2024-02-20 20:49:05 +0000

    dashboard project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+  })
+
+
+  export const lusitana = Lusitana({
+    weight: ['700'],
+    subsets: ['latin'],
+    display: 'swap',
+  })
